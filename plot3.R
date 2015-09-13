2254ec081dfbf40726118545d359622e6d3ba98a
Victor Yeh <chihkaiyeh@gmail.com>
2015-09-13 21:15:27 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -35,13 +35,13 @@ lines(datasubset$Timefram,
       type="l",
       col="red")
 
-#sub_meter3
+##sub_meter3
 lines(datasubset$Timefram,
       datasubset$Sub_metering_3,
       type="l",
       col="blue")
 
-# legend
+##legend
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
        legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
