7681b332017e52721958d384085c8388c02f845b
Victor Yeh <chihkaiyeh@gmail.com>
2015-09-13 16:28:19 +0800

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,16 @@
+data <- read.csv("household_power_consumption.txt",
+sep=";",
+dec=".", 
+stringsAsFactors= FALSE,
+na.strings = "?",
+colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric",
+"numeric", "numeric", "numeric"))
+
+dataset<- data[data$Date %in% c("1/2/2007", "2/2/2007"),] 
+str(dataset)
+
+png("plot1.png", width=520, height=520)
+plot <- as.numeric(dataset$Global_active_power)
+hist(plot, main="Global Active Power",xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+
+dev.off()
