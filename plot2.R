55d6eb52e0ea1f46704c46ed801200c487d562c1
Victor Yeh <chihkaiyeh@gmail.com>
2015-09-13 21:03:00 +0800

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,31 @@
+##Load data set
+data <- read.csv("household_power_consumption.txt",
+                 sep=";",
+                 dec=".", 
+                 stringsAsFactors= FALSE,
+                 na.strings = "?",
+                 colClasses=c("character", "character", "numeric", "numeric", "numeric", "numeric",
+                              "numeric", "numeric", "numeric"))
+
+##combine the date and time column
+data$Timefram <- strptime(paste(data$Date,data$Time),
+                          format="%d/%m/%Y %H:%M:%S")
+
+##eliminate the 
+data$Date=NULL
+data$Time=NULL 
+
+##set the desired data range
+datasubset<- subset(data,as.Date(data$Timefram) >= "2007-02-01" 
+                    & as.Date(data$Timefram) < "2007-02-03")
+
+##arrange the file name and size
+png(filename="plot2.png", height=480, width=480)
+
+plot(datasubset$Timefram,
+     datasubset$Global_active_power,
+     type="l",
+     xlab="",
+     ylab="Global Active Power (kilowatts)")
+
+dev.off()
